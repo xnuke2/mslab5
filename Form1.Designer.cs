95501a3b2255187dcab2b393199959bccc7f75cf
xnuke2 <xmnikit@gmail.com>
2025-04-16 23:46:28 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace mslab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSetIntensiv = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(764, 183);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 210);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Матрица интенсивности переходов";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(6, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(752, 132);
+            this.listBox1.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(18, 265);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(764, 186);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Уравнения Колмогорова";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(189, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(167, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Количество состояний";
+            // 
+            // buttonSetIntensiv
+            // 
+            this.buttonSetIntensiv.Location = new System.Drawing.Point(616, 14);
+            this.buttonSetIntensiv.Name = "buttonSetIntensiv";
+            this.buttonSetIntensiv.Size = new System.Drawing.Size(166, 33);
+            this.buttonSetIntensiv.TabIndex = 11;
+            this.buttonSetIntensiv.Text = "Задать интенсивность";
+            this.buttonSetIntensiv.UseVisualStyleBackColor = true;
+            this.buttonSetIntensiv.Click += new System.EventHandler(this.buttonSetIntensiv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.buttonSetIntensiv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSetIntensiv;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mslab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            int countStage =Convert.ToInt32(numericUpDown1.Value);
+            dataGridView1.Columns.Add("zero","");
+            
+            for(int i = 0; i < countStage; i++)
+                dataGridView1.Columns.Add("S" + (i + 1), "S" + (i + 1));
+            for (int i = 0; i < countStage; i++)
+                dataGridView1.Rows.Add("S" + (i + 1));
+            dataGridView1.Columns[0].ReadOnly = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1_ValueChanged(sender, e);
+        }
+
+        private void buttonSetIntensiv_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            string yr = "";
+            Func<double[], double>[] funcs = new Func<double[], double>[dataGridView1.RowCount];
+            for (int yrIndex = 0; yrIndex < dataGridView1.RowCount; yrIndex++)
+            {
+                List<Point>[] vals = new List<Point>[dataGridView1.RowCount];
+                yr = "";
+                vals[yrIndex] = new List<Point>();
+                for (int rowIndex = 0; rowIndex < dataGridView1.RowCount; rowIndex++)
+                        if (Convert.ToString(dataGridView1[ yrIndex+1, rowIndex].Value) != "")
+                        {
+                            yr += Convert.ToString(dataGridView1[yrIndex + 1, rowIndex].Value) + "*S" + (yrIndex + 1) + "+";
+                            vals[yrIndex].Add(new Point(yrIndex + 1, rowIndex));
+                        }
+                            
+                if(yr!="")
+                    yr =yr.Remove(yr.Length-1);
+                
+                for (int columnsIndex = 0; columnsIndex < dataGridView1.Rows.Count; columnsIndex++)
+                    if (yrIndex != columnsIndex)
+                        if (Convert.ToString(dataGridView1[columnsIndex+1, yrIndex].Value) != "")
+                        {
+                            vals[columnsIndex]=new List<Point>();
+                            vals[columnsIndex].Add(new Point(columnsIndex + 1, yrIndex));
+                            yr += "-" + Convert.ToString(dataGridView1[columnsIndex + 1, yrIndex].Value) + "*S" + (columnsIndex + 1);
+
+                        }
+                double[] ratio = new double[vals.Length];
+                for (int i = 0; i < vals.Length; i++)
+                {
+                    ratio[0] = 0;
+                    if(vals[i]!=null)
+                    for(int j=0; j < vals[i].Count; j++)
+                    {
+                        ratio[0] += Convert.ToDouble(dataGridView1[vals[i][j].X, vals[i][j].Y].Value);
+                    }
+                    for(int j=1;j<vals.Length; j++)
+                        if (vals[j] !=null&& vals[j].Count>0)
+                        {
+                            ratio[j]= -Convert.ToDouble(dataGridView1[vals[j][0].X, vals[j][0].Y].Value);
+                        }
+
+
+                }
+                funcs[yrIndex] = Yvals =>
+                {
+                    double sum = 0;
+                    for (int j = 0; j < ratio.Length&&j<Yvals.Length; j++)
+                        sum += ratio[j] * Yvals[j];
+                    return sum;
+                };
+
+                listBox1.Items.Add(yr);
+            }
+            
+
+            
+
+
+        }
+    }
+}
